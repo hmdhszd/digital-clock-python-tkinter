98fdfdfb9f4f5d899e139434adbb1572ab9ea8f4
Hamid Hosseinzadeh <hamidhszd@gmail.com>
2020-01-29 09:32:09 +0330

    updated

--- Digital Clock Python (tkinter).py
+++ Digital Clock Python (tkinter).py
@@ -3,47 +3,20 @@ from tkinter.ttk import *
 from time import strftime 
 
 
+
+  
+
 clock = Tk()
-clock.title("Digital Clock Python (tkinter)")
-lbl = Label(clock, text=time.strftime("%H:%M:%S") , font=("Arial Bold", 50))
-lbl.grid(column=0, row=0)
-clock.mainloop()
+clock.title('Digital Clock Python (tkinter)') 
 
-while True:
-    lbl.config(text=time.strftime("%H:%M:%S"))
-    lbl.grid(column=0, row=0)
-
-
-
-import time
-print("something")
-time.sleep(5.5)    # pause 5.5 seconds
-print("something")
-
-
-
-
-  
-
-root = Tk() 
-root.title('Clock') 
-  
-# This function is used to  
-# display time on the label 
 def time(): 
     string = strftime('%H:%M:%S %p') 
     lbl.config(text = string) 
     lbl.after(1000, time) 
   
-# Styling the label widget so that clock 
-# will look more attractive 
-lbl = Label(root, font = ('calibri', 40, 'bold'), 
-            background = 'black', 
-            foreground = 'red') 
-  
-# Placing clock at the centre 
-# of the tkinter clock 
+lbl = Label(clock, font = ('arial', 60, 'bold'), background = 'black', foreground = 'red') 
 lbl.pack(anchor = 'center') 
+
 time() 
   
 mainloop() 
